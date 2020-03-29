a852a9a34172d35c98720b7388e90774ef1e2aac
github username <github email address>
2020-03-29 19:12:53 -0400

    corrected syntax

---
+++ pet-me-frontend/build/precache-manifest.c3cc63db55f69b476e2b3f3748898356.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86972d242f52cca970405a9732afc0af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29aa55ab952f85776b8e",
+    "url": "/static/css/main.4303b419.chunk.css"
+  },
+  {
+    "revision": "7cbb2d15664264cdd1ee",
+    "url": "/static/js/2.8a40f216.chunk.js"
+  },
+  {
+    "revision": "29aa55ab952f85776b8e",
+    "url": "/static/js/main.b85a75e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
